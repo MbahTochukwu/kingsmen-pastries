4b5be6fd2c8b1a3e1f63247cbfa03a348584d5aa
Mbah Tochukwu <mbahtochukwu75@gmail.com>
2025-07-16 00:59:07 +0100

    order model

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { protect, admin } = require('../middleware/authMiddleware');
-const Order = require('../models/Order');
+const Order = require('../models/order');
 
 router.get('/', async (req, res) => {
   try {
