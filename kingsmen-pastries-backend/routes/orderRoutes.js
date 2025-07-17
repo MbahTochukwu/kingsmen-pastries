c4fb153d24b5c3b455b8c8ce22d4a2f9dd2f65bb
Mbah Tochukwu <mbahtochukwu75@gmail.com>
2025-07-17 12:04:19 +0100

    fix import path for authMiddleware

--- kingsmen-pastries-backend/routes/orderRoutes.js
+++ kingsmen-pastries-backend/routes/orderRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { protect, admin } = require('../kingsmen-pastries-backend/middleware/authMiddleware');
-const Order = require('../kingsmen-pastries-backend/models/order');
+const { protect, admin } = require('../middleware/authMiddleware');
+const Order = require('../models/order');
 
 router.get('/', async (req, res) => {
   try {
