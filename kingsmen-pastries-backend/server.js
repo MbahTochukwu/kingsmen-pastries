b7d718050639473203dc9ee50751d9e2a0b91729
Mbah Tochukwu <mbahtochukwu75@gmail.com>
2025-07-17 10:42:37 +0100

    Fix CORS to allow all origins

--- kingsmen-pastries-backend/server.js
+++ kingsmen-pastries-backend/server.js
@@ -3,10 +3,6 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 
-// app.use(cors({
-//   origin: ['http://127.0.0.1:5501', 'http://localhost:5501', 'https://kingsmen-pastries.onrender.com'],
-//   credentials: true
-// }));
 
 app.use(cors({
   origin: "*",
